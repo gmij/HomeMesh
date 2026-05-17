a3044264f95adedf23e78e7a696c156953064531
Gmij <gmij@qq.com>
2026-05-17 23:16:11 +0800

    feat: add application service registration

---
+++ src/HomeMesh.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using HomeMesh.Application.Members;
+using HomeMesh.Application.NetworkConfig;
+using HomeMesh.Application.Networks;
+using HomeMesh.Application.Setup;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeMesh.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddHomeMeshApplication(this IServiceCollection services)
+    {
+        services.AddScoped<SetupService>();
+        services.AddScoped<NetworkService>();
+        services.AddScoped<RouteService>();
+        services.AddScoped<IpPoolService>();
+        services.AddScoped<DnsConfigService>();
+        services.AddScoped<MemberService>();
+        return services;
+    }
+}
