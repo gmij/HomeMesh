042141cd50e2edf9ab9e4520b7076d3aa7989f3b
Gmij <gmij@qq.com>
2026-05-17 22:47:34 +0800

    feat: register zerotier provider

---
+++ src/HomeMesh.Protocol.ZeroTier/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+using HomeMesh.Abstractions.Providers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeMesh.Protocol.ZeroTier;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddZeroTierProvider(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ZeroTierOptions>(configuration.GetSection("Providers:ZeroTier"));
+        services.AddSingleton<ZeroTierControllerProvider>();
+        services.AddSingleton<ISdwanControllerProvider>(sp => sp.GetRequiredService<ZeroTierControllerProvider>());
+        return services;
+    }
+}
